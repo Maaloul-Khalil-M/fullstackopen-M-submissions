3ac3f14a8e0b465c66407b040a68a64940e84d05
Maaloul Khalil M <maaloul.mk5@gmail.com>
2026-04-17 02:05:37 +0100

    exercice 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -17,7 +17,6 @@ const Content = ({ parts }) => {
     _parts.push(part.name);
     exercises.push(part.exercises);
   }
-
   return (
     <div>
       <Part part={_parts[0]} exercise={exercises[0]} />
@@ -41,27 +40,29 @@ const Total = ({ parts }) => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
