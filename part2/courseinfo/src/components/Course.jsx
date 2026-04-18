4c308106b7b98f1dc1b4b2fe9c189cd77d2f5257
Maaloul Khalil M <maaloul.mk5@gmail.com>
2026-04-18 03:17:09 +0100

    exercice 2.5

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,34 @@
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+const Header = (props) => <h1>{props.course}</h1>;
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <>
+    {parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </>
+);
+
+const Total = ({ parts }) => {
+  const totalExercises = parts.reduce((sum, part) => {
+    return sum + part.exercises;
+  }, 0);
+  return <strong>Number of {totalExercises} exercises</strong>;
+};
+
+export default Course;
